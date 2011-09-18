80a915f73e5bbd73014acd68c1a367646d459d7c
Rishab Arora <spacetime@Rishab-Aroras-MacBook-Pro.local>
2011-09-18 21:51:24 +0530

    Initial Commit

---
+++ CSTT/CSTTAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CSTTAppDelegate.h
+//  CSTT
+//
+//  Created by Rishab Arora on 18/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSTTAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ CSTT/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CSTT
+//
+//  Created by Rishab Arora on 18/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
